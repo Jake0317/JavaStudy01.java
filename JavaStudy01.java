0a909bd9ca4c5cefa117dfb0941ab817653ede5c
Jake0317 <jgk0318@gmail.com>
2024-06-09 17:19:34 +0900

    Update JavaStudy01.java

--- JavaStudy01.java
+++ JavaStudy01.java
@@ -1,12 +1,27 @@
-public class StandardIO {
-	public static void main(String[] args) throws IOException {
+public class Main{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
 
-   //int를 입력 받을 때는 nextInt 메서드를 이용한다.
-		Scanner sc = new Scanner(System.in);
-		int a,b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a+" "+b);
-	}
+		System.out.print("<<<<메뉴선택>>>>")
+		System.out.print("1.회원 관리")
+		System.out.print("2.과목 관리")
+		System.out.print("3.수강 관리")
+		System.out.print("4.결제 관리")
+		System.out.print("5.종료")
+
+	   	int num = input.nextInt();
+		if (num = 1) {
+			System.out.println("회원 관리 메뉴를 선택했습니다.");}
+		if (num = 2) {
+			System.out.println("과목 관리 메뉴를 선택했습니다.")}
+		if (num = 3) {
+			System.out.println("수강 관리 메뉴를 선택했습니다.")}
+		if (num = 4) {
+			System.out.println("결제 관리 메뉴를 선택했습니다.")}
+		if (num = 5) {
+			System.out.println("프로그램을 종료합니다.")}
+		else {
+			System.out.println("입력값이 정확하지 않습니다.")}
+			}
 }
 
